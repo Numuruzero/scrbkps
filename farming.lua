38c30336771a3985547403c493e283bdee230ff9
Numuruzero <35202735+Numuruzero@users.noreply.github.com>
2026-02-23 13:28:58 -0600

    Add movement logic function

--- farming.lua
+++ farming.lua
@@ -13,6 +13,28 @@ function farmWheat()
     end
 end
 
+function decideMove()
+    local success, data = turtle.inspectUp()
+    if success then
+        if data.name == "minecraft:stone" then
+            turtle.forward()
+        elseif data.name == "minecraft:chest" then
+            -- Refuel logic here
+            turtle.forward()
+        elseif data.name == "minecraft:diorite" then
+            turtle.turnRight()
+            turtle.forward()
+        elseif data.name == "minecraft:andesite" then
+            turtle.turnLeft()
+            turtle.forward()
+        else
+            print("Unknown block above: " .. data.name)
+        end
+    else
+        print("Nothing above to inspect.")
+    end
+end
+
 while true do
     farmWheat()
     os.sleep(10) -- Wait for 10 seconds before checking again
