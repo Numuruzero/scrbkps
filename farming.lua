618ba8030d16aafbdb545c266a6c3ab5defdaf31
Numuruzero <35202735+Numuruzero@users.noreply.github.com>
2026-02-25 11:24:45 -0600

    Add logic for refueling, replanting, reorganized functions

--- farming.lua
+++ farming.lua
@@ -1,15 +1,35 @@
-function farmWheat()
-    local success, data = turtle.inspectDown()
-    if success then
-        if data.name == "minecraft:wheat" and data.state.age == 7 then
-            turtle.digDown()
-            turtle.suckDown()
-            turtle.placeDown()
+function findFuel()
+    for slot = 1, 16 do
+        local item = turtle.getItemDetail(slot)
+        if item and (item.name == "minecraft:coal" or item.name == "minecraft:charcoal") then
+            return slot
+        end
+    end
+    return nil -- No fuel found
+end
+
+function refuelIfNeeded()
+    local fuelLevel = turtle.getFuelLevel()
+    if fuelLevel < 100 then -- Arbitrary threshold for refueling
+        print("Fuel level low: " .. fuelLevel .. ". Attempting to refuel.")
+        -- Refueling logic here, e.g. checking inventory for fuel items and using them
+        local fuelSlot = findFuel()
+        if fuelSlot then
+            turtle.select(fuelSlot)
+            turtle.refuel()
+            print("Refueled using slot " .. fuelSlot .. ". New fuel level: " .. turtle.getFuelLevel())
         else
-            print("Not fully grown wheat below.")
+            turtle.suckUp() -- Try to suck fuel from above if available
+            fuelSlot = findFuel()
+            if fuelSlot then
+                turtle.select(fuelSlot)
+                turtle.refuel()
+                print("Refueled using slot " .. fuelSlot .. " after sucking up. New fuel level: " .. turtle.getFuelLevel())
+            else
+                print("No fuel found in inventory or above. Please refuel manually.")
         end
     else
-        print("Nothing below to inspect.")
+        print("Fuel level sufficient: " .. fuelLevel)
     end
 end
 
@@ -20,6 +40,7 @@ function decideMove()
             turtle.forward()
         elseif data.name == "minecraft:chest" then
             -- Refuel logic here
+            refuelIfNeeded()
             turtle.forward()
         elseif data.name == "minecraft:diorite" then
             turtle.turnRight()
@@ -27,11 +48,47 @@ function decideMove()
         elseif data.name == "minecraft:andesite" then
             turtle.turnLeft()
             turtle.forward()
-        else
+        else -- Add another block to indicate that items should be dropped into a chest ahead
             print("Unknown block above: " .. data.name)
         end
     else
         print("Nothing above to inspect.")
+    end
+end
+
+function findSeed(plantType)
+    -- Wheat has seeds, but other plants like carrots and potatoes are planted using the item itself, so we need to adjust for that
+    if plantType == "minecraft:wheat" then
+        plantType = "minecraft:wheat_seeds"
+    end
+    for slot = 1, 16 do
+        local item = turtle.getItemDetail(slot)
+        if item and item.name == plantType then
+            return slot
+        end
+    end
+    return nil -- No seeds found
+end
+
+function farmWheat()
+    local success, data = turtle.inspectDown()
+    if success then
+        -- TODO: Add support for other crops, which may require checking for different growth stages. Maybe able to use some kind of "farmable" tag?
+        if data.name == "minecraft:wheat" and data.state.age == 7 then
+            turtle.digDown()
+            turtle.suckDown()
+            local seedSlot = findSeed(data.name)
+            if seedSlot then
+                turtle.select(seedSlot)
+                turtle.placeDown()
+            else
+                print("No seeds found in inventory to replant.")
+            end
+        else
+            print("Not fully grown wheat below.")
+        end
+    else
+        print("Nothing below to inspect.")
     end
 end
 
@@ -61,7 +118,7 @@ end
 -- Seeing inventory on a PC, e.g.
 -- peripheral.call("right","list")
 -- Return e.g. {{count = 1, name = "minecraft:wheat"},{count = 1, name = "minecraft:wheat_seeds"},nil,{count = 1, name = "minecraft:dirt"}}
--- If I understand correctly this should mean that the key corresponds to the actual slot in the inventory
+-- If I understand correctly this should mean that the key corresponds to the actual slot in the inventory and the system seems to replace a single empty slot with nil, but will ignore multiple empty slots and instead give the next item the key for its slot
 -- Two or more computers can connect to the same peripheral
 
 -- For turtles:
